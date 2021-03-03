08ece524166637e5df331a8659004552ca1a166c
Marcos Sibilio Jr <marcos.sibilio@multpex.com.br>
2021-03-03 14:54:12 -0300

    Primeiro commit

---
+++ src/test/java/br/com/mcscloud/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mcscloud.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
