cb2ccd7f0f7db6e68b2545c189b51dbf6553ef05
Marcos Sibilio Jr <marcos.sibilio@multpex.com.br>
2021-03-04 12:20:34 -0300

    criação dos modelos de banco de dados

---
+++ src/main/java/br/com/mcscloud/bookstoremanager/books/entity/Book.java
@@ -0,0 +1,38 @@
+package br.com.mcscloud.bookstoremanager.books.entity;
+
+import br.com.mcscloud.bookstoremanager.author.entity.Author;
+import br.com.mcscloud.bookstoremanager.publishers.entity.Publisher;
+import br.com.mcscloud.bookstoremanager.users.entity.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 35)
+    private String isbn;
+
+    @Column(columnDefinition = "integer default 0")
+    private int pages;
+
+    @Column(columnDefinition = "integer default 0")
+    private int chapters;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Author author;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Publisher publisher;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private User user;
+}
---
+++ src/main/java/br/com/mcscloud/bookstoremanager/users/enums/Gender.java
@@ -0,0 +1,12 @@
+package br.com.mcscloud.bookstoremanager.users.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender {
+    MALE("Male"), FEMALE("Female");
+
+    private String description;
+}
